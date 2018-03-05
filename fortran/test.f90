f7181af9ae3681f72d863b7ca431c1eae2292215
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-05 13:42:52 -0500

    init

---
+++ fortran/test.f90
@@ -0,0 +1,16 @@
+program basictest
+implicit none
+
+logical, parameter :: jf(50) = .true.
+integer, parameter :: jmag = 1, iyyyy=2012,mmdd=0821,dhour=12
+real, parameter :: glat=40., glon=-100.
+real,parameter :: altkm(1) = 130.
+integer,parameter ::  Nalt = size(altkm)
+
+real :: oarr(100), outf(30,Nalt)
+
+
+call IRI_SUB(JF,JMAG,glat,glon,IYYYY,MMDD,DHOUR+25, altkm,Nalt,OUTF,OARR)
+
+
+end program
