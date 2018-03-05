2a9218e9cbf45b1974f05531107ae77b7380e9f4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-05 16:27:55 -0500

    import syntax

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
-from pyiri2016 import IRI2016
+import pyiri2016
 
 def test_main1():
 
-    iri = IRI2016().IRI('1980-03-21T12', 130., 0., 0.)
+    iri = pyiri2016.IRI('1980-03-21T12', 130., 0., 0.)
 
     np.testing.assert_allclose((iri.loc[:,'ne'].item(), iri.attrs['NmF2'], iri.attrs['hmF2']),
                     (267285184512.0, 2580958937088.0, 438.78643798828125))
